6e62ac3d3d56560b1469e0716916c05b6a2a5353
Moein <moeinkhanmohamadi3@gmail.com>
2025-12-12 21:26:45 +0330

    Character Movement added

---
+++ Assets/Scripts/Batman_Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    public float moveSpeed = 5f;
+    [SerializeField]
+    public float turnSpeed = 10f;
+    [SerializeField]
+    public float gravity = -9.8f;
+    [SerializeField]
+    public float jumpHeight = 2f;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Input
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(h, 0, v).normalized;
+
+        // Sprint with holding LeftShift
+        float currentSpeed = moveSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed *= 2;
+        }
+
+        // Calculate Movement and Rotation
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.Euler(0, targetAngle, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * turnSpeed);
+
+            controller.Move(transform.forward * currentSpeed * Time.deltaTime);
+        }
+
+        // Gravity
+        controller.Move(Vector3.up * gravity * Time.deltaTime);
+    }
+}
